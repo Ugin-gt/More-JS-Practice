2323d50be2485e5875329b231c200091afa7ee3c
Ugin-gt <apach575@gmail.com>
2021-01-27 13:56:51 +0200

    Add 4-5 tasks

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function sumFun(n) {
 const f = 5;
 console.log(sumFun(f));
 
-// 3.3 Вывести только четные элементы (четные числа делятся на 2 без остатка)
-// 3.4 Вывести индексы нулевых элементов (элемент равен нулю)
-// 3.5 Подсчитать количество нулевых элементов
 
 // 2.1 Создать объект Student который содержит следующие свойства: имя, фамилию, пол, контактные данные.
 
@@ -45,11 +42,69 @@ console.log(student);
 // 3.1*Инициализация с помощью случайных чисел
 
 const array = new Array(25);
-const newarr = array.fill(0).map(() => (Math.ceil(Math.random()*100))) 
+const newarr = array.fill().map(() => (Math.ceil(Math.random() * 100)));
+console.log(newarr);
 
 // 3.2 Вывести элементы с четными индексами
 
-console.log(newarr.filter(function(el,index){(index%2===0)}));
+const evenArrInd = newarr.filter((value, index) => index % 2 === 0);
+console.log(evenArrInd);
 
+// 3.3 Вывести только четные элементы (четные числа делятся на 2 без остатка)
 
+const evenArrVal = newarr.filter((value, index) => value % 2 === 0);
+console.log(evenArrVal);
 
+// 3.4 Вывести индексы нулевых элементов (элемент равен нулю)
+
+const nullArr = [2, 4, 5, 0, 2, 0, 35, 0, 13, 0];
+const nullIndex = [];
+for (let i = 0; i <= nullArr.length; ++i) {
+  if (nullArr[i] === 0) { nullIndex.push(i); }
+};
+console.log(nullIndex);
+
+// 3.5 Подсчитать количество нулевых элементов
+
+const nullVal = nullArr.filter((value) => value === 0);
+console.log(nullVal.length);
+
+// 4 Создать классы:
+// - Книга (автор, название, год издания, издательство)
+// - Электронная версия книги (автор, название, год издания, издательство, формат, электронный номер)
+
+class Book {
+  constructor(author, title, yearOfPub, publishHouse) {
+    this._author = author;
+    this._title = title;
+    this._yearOfPub = yearOfPub;
+    this._publisHouse = publishHouse;
+  }
+}
+
+class Ebook extends Book {
+  constructor(author, title, yearOfPub, publishHouse, publishFormat, eNumber) {
+    super(author, title, yearOfPub, publishHouse);
+    this._publishFormat = publishFormat;
+    this.eNumber = eNumber;
+  }
+}
+// 5 Требуется написать функцию, выводящую в консоль числа от 1 до n, где n — это целое число, которая функция принимает в качестве параметра, с такими условиями:
+// вывод fizzbuzz вместо чисел, кратных как 3, так и 5.
+// вывод fizz вместо чисел, кратных 3;
+// вывод buzz вместо чисел, кратных 5;
+
+function printNumbers2(n) {
+  new Array(n).fill(0).map((e, i) => e = i).map((e) => console.log(compare(e)));
+  function compare(e) {
+    if ((e % 3 === 0) && (e % 5 === 0)) {
+      return ('fizzbuzz');
+    } else if (e % 5 === 0) {
+      return ('buzz');
+    } else if (e % 3 === 0) {
+      return ('fizz');
+    } else {
+      return e;
+    }
+  }
+}
