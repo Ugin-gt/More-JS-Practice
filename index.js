6a022c25833b587d1d9499fc19e2981729ccb40a
Ugin-gt <apach575@gmail.com>
2021-01-28 03:23:32 +0200

    Add 5. task change 2. task

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 // 1 Вычислить сумму первых N элементов последовательности . параметр N задает пользователь
 // (например n=4 , 1+2+3+4)
 
@@ -15,28 +15,46 @@ console.log(sumFun(f));
 
 // 2.1 Создать объект Student который содержит следующие свойства: имя, фамилию, пол, контактные данные.
 
-const student = {
-  name: "Вася",
-  surname: "Иванов",
-  isMale: true,
-  email: "vanyaI@mail.com",
-  phoneNum: "+380675741812",
-};
+const genderSelect = ['male', 'female', 'other'];
+class Student {
+  constructor(name, surname, gender, email, phoneNum) {
+    this._name = name;
+    this._surname = surname;
+    this._gender = gender;
+    this._email = email;
+    this._phoneNum = phoneNum;
 
+  }
+  set gender(v) {
+    if (genderSelect.includes(v)) {
+      this._gender = (v);
+    } else {
+      throw new RangeError('Choose: male, female, other');
+    }
+  };
+  get gender() {
+    return this._gender
+  }
+};
 // 2.2 Создать объект, который содержит свойства, о факультете и кафедре.
-const university = {
-  nameUni: "Национальный политехнический университет",
-  faculty: "Экономический факультет",
-  chair: "Кафедра Экономической кибернетики",
-};
-
 // 2.3 Связать объекты между собой. т.е. прописать данные об факультете и кафедре для студента
+class University extends Student {
+  constructor(name, surname, gender, email, phoneNum, nameUni, faculty, chair) {
+    super(name, surname, gender, email, phoneNum);
+    this._nameUni = nameUni;
+    this._faculty = faculty;
+    this._chair = chair;
+  }
 
-student.getUniInfo = university;
+  // 2.4 Реализовать функцию выводит на экран всю информацию о студенте
+  getFullInfo() {
+    return Object.entries(this);
+  }
+}
 
-// 2.4 Реализовать функцию выводит на экран всю информацию о студенте
+const student1 = new University("Вася", "Иванов", "male", "vanyaI@mail.com", "+380675741812", "Национальный политехнический университет", "Экономический факультет", "Кафедра Экономической кибернетики");
 
-console.log(student);
+console.log(student1.getFullInfo())
 
 // 3.1 Создать числовой массив и проинициализировать его из 25 элементов.
 // 3.1*Инициализация с помощью случайных чисел
@@ -57,16 +75,16 @@ console.log(evenArrVal);
 
 // 3.4 Вывести индексы нулевых элементов (элемент равен нулю)
 
-const nullArr = [2, 4, 5, 0, 2, 0, 35, 0, 13, 0];
+const multArr = [2, 4, 5, 0, 2, 0, 35, 0, 13, 0];
 const nullIndex = [];
-for (let i = 0; i <= nullArr.length; ++i) {
-  if (nullArr[i] === 0) { nullIndex.push(i); }
+for (let i = 0; i < multArr.length; ++i) {
+  if (multArr[i] === 0) { nullIndex.push(i); }
 };
 console.log(nullIndex);
 
 // 3.5 Подсчитать количество нулевых элементов
 
-const nullVal = nullArr.filter((value) => value === 0);
+const nullVal = multArr.filter((value) => value === 0);
 console.log(nullVal.length);
 
 // 4 Создать классы:
@@ -81,7 +99,6 @@ class Book {
     this._publisHouse = publishHouse;
   }
 }
-
 class Ebook extends Book {
   constructor(author, title, yearOfPub, publishHouse, publishFormat, eNumber) {
     super(author, title, yearOfPub, publishHouse);
@@ -89,11 +106,33 @@ class Ebook extends Book {
     this.eNumber = eNumber;
   }
 }
-// 5 Требуется написать функцию, выводящую в консоль числа от 1 до n, где n — это целое число, которая функция принимает в качестве параметра, с такими условиями:
+// 5 Требуется написать функцию, выводящую в консоль числа от 1 до n, 
+// где n — это целое число, которая функция принимает в качестве параметра, с такими условиями:
 // вывод fizzbuzz вместо чисел, кратных как 3, так и 5.
 // вывод fizz вместо чисел, кратных 3;
 // вывод buzz вместо чисел, кратных 5;
 
+function multiplesNumToStr(n) {
+  // debugger
+  const multArr = new Array(n).fill().map(() => (Math.ceil(Math.random() * (n) + 1)));
+  const resultStr = [];
+  for (let i = 0; i < multArr.length; ++i) {
+    if (multArr[i] % 3 === 0 && multArr[i] % 5 === 0) {
+      resultStr.push(`fizzbuzz`);
+    } else if (multArr[i] % 3 === 0) {
+      resultStr.push(`fizz`);
+    } else if (multArr[i] % 5 === 0) {
+      resultStr.push('buzz');
+    } else {
+      resultStr.push(multArr[i]);
+    }
+  }
+  console.log(multArr);
+  console.log(resultStr);
+}
+
+
+// Об этом варианте пока приходится только мечтать)))
 function printNumbers2(n) {
   new Array(n).fill(0).map((e, i) => e = i).map((e) => console.log(compare(e)));
   function compare(e) {
